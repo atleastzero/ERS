949098836d96b3130718509422aaabec6bb6e7a4
atleastzero <megan.obryan52@gmail.com>
2021-10-08 21:31:02 -0700

    Trying to redirect to room

--- index.js
+++ index.js
@@ -5,12 +5,19 @@ const server = http.createServer(app)
 const { Server } = require('socket.io')
 const io = new Server(server)
 
+let rooms = {};
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html')
 })
 
-io.on('connection', (socket) => {
-  console.log('a user connected')
+app.get('/new', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+app.get('/play', (req, res) => {
+  let room = req.body.room
+  console.log(room)
 })
 
 server.listen(3000, () => {});
